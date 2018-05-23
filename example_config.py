b3cfe90390d1b8eccc5e2ac836b65b1d1332a12f
Wolf Vollprecht <w.vollprecht@gmail.com>
2018-05-23 14:53:12 +0200

    re-add example config

---
+++ example_config.py
@@ -0,0 +1,19 @@
+# GRAPHITE
+GRAPHITE_SERVER = ''
+
+# GIST
+GIST_URL = 'https://api.github.com'
+GIST_USER = ''
+GIST_API_TOKEN = ''
+
+# MAIL
+MAIL_RECEIVER = [''] 
+
+MAIL_SENDER = ''    # you can use your personal gmail account here if you like
+SMTP_PASSWORD = ''
+
+SMTP_SERVER = 'smtp.gmail.com'
+SMTP_PORT = 587
+
+import socket
+HOSTNAME = socket.gethostname()
